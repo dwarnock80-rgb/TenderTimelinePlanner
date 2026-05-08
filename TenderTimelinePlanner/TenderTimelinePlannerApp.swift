8cd628281ebcbd59cd29445ae55058458a693dd1
dwarnock80-rgb <d.warnock80@gmail.com>
2026-05-08 10:08:54 +0100

    Initial Commit

---
+++ TenderTimelinePlanner/TenderTimelinePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenderTimelinePlannerApp.swift
+//  TenderTimelinePlanner
+//
+//  Created by Daniel Warnock on 08/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TenderTimelinePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
